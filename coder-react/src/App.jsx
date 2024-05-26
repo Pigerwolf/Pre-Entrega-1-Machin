ad203671a764c106182ac9bc2a858a60aa94adbc
Hendersson Machin <henderssonconh@gmail.com>
2024-05-26 16:34:28 +0200

    Primer Commit, desde la terminal de GitBash

---
+++ coder-react/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Header from './components/Header';
+import Logo from './components/Logo';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Logo />
+      <MainContent />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
---
+++ coder-react/src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import './index.css';
+import { createRoot } from 'react-dom/client';
+
+
+createRoot(document.getElementById('root')).render(<App />);
