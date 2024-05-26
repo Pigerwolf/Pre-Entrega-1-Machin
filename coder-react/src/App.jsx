326bcb326b25b1e209beeac8753b32671497f610
Hendersson Machin <henderssonconh@gmail.com>
2024-05-26 19:58:47 +0200

    Se agregaron la importanción de los Estilos de Css correspondientes.

--- coder-react/src/App.jsx
+++ coder-react/src/App.jsx
@@ -5,6 +5,7 @@ import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import "./App.css"
 
 const App = () => {
   return (
