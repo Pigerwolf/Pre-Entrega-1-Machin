5348f861d1c8fd85132b02ec89d0bc7aba747e4e
Hendersson Machin <henderssonconh@gmail.com>
2024-06-09 18:07:12 +0200

    Comienzo de enrutamiento

--- coder-react/src/App.jsx
+++ coder-react/src/App.jsx
@@ -6,15 +6,23 @@ import Footer from './components/Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import "./App.css"
+import { BrowserRouter, Routes, Route }  from 'react-router-dom';
 
 const App = () => {
   return (
     <div>
+      <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<home />}>
+      </Route>
+      </Routes>
+      </BrowserRouter>
       <Header />
       <Nav-bar/>
       <Logo />
       <MainContent />
       <Footer />
+
     </div>
   );
 };
--- coder-react/src/components/Nav-Bar.jsx
+++ coder-react/src/components/Nav-Bar.jsx
@@ -18,6 +18,29 @@ const NavBar = () => {
           </button>
           <div className="collapse navbar-collapse pl-4" id="navbarNav">
             <ul className="navbar-nav">
+            <div class="container">
+  <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img class="d-block w-100 img-fluid" src="https://images.ctfassets.net/1nw0m35kh5t7/6kI0OuYTrVywhKOyUd1kDi/ed6186f9da8feb4464ec6f9e238e6073/Types-of-e-commerce-16-9" alt="First slide"/>
+      </div>
+      <div class="carousel-item img-fluid">
+        <img class="d-block w-100" src="https://assets.entrepreneur.com/content/3x2/2000/20191127134656-e-commerce-3406613.jpeg?width=600&crop=16:9" alt="Second slide"/>
+      </div>
+      <div class="carousel-item img-fluid">
+        <img class="d-block w-100" src="https://media.newyorker.com/photos/5b465113da42df0b560981d3/16:9/w_1280,c_limit/180723_r32444_rd.jpg" alt="Third slide"/>
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Anterior</span>
+    </a>
+    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Siguiente</span>
+    </a>
+  </div>
+ </div>
               {/* Menú desplegable */}
               <li className="nav-item dropdown">
                 <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
